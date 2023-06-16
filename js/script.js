288342c2e21bd833dcd57243df932f73ea44c476
Michael Angelo Dorio <michaelangelo.dorio@gmail.com>
2023-06-17 05:03:43 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const typed = new Typed('.typewriting-text', {
+    strings: [
+    'TechSupport Engineer',
+  
+    'Frontend Developer',
+    'Team Player',
+    'Game Developer'],
+    typeSpeed: 60,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true
+});
